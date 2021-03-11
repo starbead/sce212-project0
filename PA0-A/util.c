4bc789672ac253ea41fa0bfede4b0c3494ccd7ed
Jonghyeon Kim <tome01@ajou.ac.kr>
2021-03-11 04:46:51 +0000

    first commit

---
+++ PA0-A/util.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "util.h"
+
+int is_large(int num1, int num2)
+{
+	return num1 > num2;
+}
+
+int sum_x(int x1, int x2)
+{
+	int sum = 0;
+	/* Fill this function */
+	return sum;
+}
+
+void sub_y(int y1, int y2, int *sub)
+{
+	/* Fill this function */
+}
+
+// You have to allocate memory for pointer members of "struct Point_ref"
+// Hint: Use malloc()
+void Point_val_to_Point_ref(struct Point_val *P1, struct Point_ref *P2)
+{
+	/* Fill this function */
+}
+
+void Point_ref_to_Point_val(struct Point_ref *P1, struct Point_val *P2)
+{
+	/* Fill this function */
+}
+
+int calc_area1(struct Point_val *P1, struct Point_val *P2)
+{
+	/* Fill this function */
+	int area = 0;
+	return area;
+}
+
+void calc_area2(struct Point_ref *P1, struct Point_ref *P2, int *area)
+{
+	/* Fill this function */
+}
+
+char* reverse(char *word)
+{
+	/* Fill this function */
+	return word;
+}
---
+++ PA0-B/utils.c
@@ -0,0 +1,33 @@
+#include "dir_file.h"
+#include "utils.h"
+
+int open_file(char* fname, FILE** input) {
+    if (access(fname, F_OK) == -1) {
+        ERR_PRINT("The '%s' file does not exists\n", fname);
+        return -1;
+    }
+
+    *input = fopen(fname, "r");
+    if (input == NULL) {
+        ERR_PRINT("Failed open '%s' file\n", fname);
+        return -1;
+    }
+
+    return 1;
+}
+
+// This parse_str_to_list() split string to the tokens, and put the tokens in token_list.
+// The value of return is the number of tokens.
+int parse_str_to_list(const char* str, char** token_list) {
+    /* Fill this function */
+}
+
+void free_token_list(char** token_list, int num_token) {
+    int index;
+
+    for (index = 0; index < num_token; index++) {
+        free(token_list[index]);
+    }
+
+    free(token_list);
+}
